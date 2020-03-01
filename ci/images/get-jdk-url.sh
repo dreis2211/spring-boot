0decccb076215d934e435e087573aec2b3e7efde
dreis2211 <christoph.dreis@freenet.de>
2020-03-01 20:45:37 +0100

    Add Java 14 CI

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -11,6 +11,9 @@ case "$1" in
 	java13)
 		 echo "https://github.com/AdoptOpenJDK/openjdk13-binaries/releases/download/jdk-13.0.2%2B8/OpenJDK13U-jdk_x64_linux_hotspot_13.0.2_8.tar.gz"
 	;;
+	java14)
+		 echo "https://github.com/AdoptOpenJDK/openjdk14-binaries/releases/download/jdk14-2020-02-26-04-25/OpenJDK14-jdk_x64_linux_hotspot_2020-02-26-04-25.tar.gz"
+	;;
   *)
 		echo $"Unknown java version"
 		exit 1
--- ci/scripts/build-project.sh
+++ ci/scripts/build-project.sh
@@ -5,5 +5,9 @@ source $(dirname $0)/common.sh
 repository=$(pwd)/distribution-repository
 
 pushd git-repo > /dev/null
-./gradlew -Dorg.gradle.internal.launcher.welcomeMessageEnabled=false --no-daemon --max-workers=4 -PdeploymentRepository=${repository} build publishAllPublicationsToDeploymentRepository
+if [[ -d /opt/openjdk-secondary ]]; then
+	./gradlew -Dorg.gradle.internal.launcher.welcomeMessageEnabled=false --no-daemon --max-workers=4 -PtestJavaHome=/opt/openjdk-secondary -PdeploymentRepository=${repository} build publishAllPublicationsToDeploymentRepository
+else
+	./gradlew -Dorg.gradle.internal.launcher.welcomeMessageEnabled=false --no-daemon --max-workers=4 -PdeploymentRepository=${repository} build publishAllPublicationsToDeploymentRepository
+fi
 popd > /dev/null
